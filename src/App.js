4b64aede50f051ed350d15c4fc6a93bf38f400d8
zackvuldin <y.zachary@gmail.com>
2020-10-20 22:18:22 -0700

    frontend commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Registrant from './components/Registrant';
+import { Route, Switch, Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<nav>
+					<Link to={'/registrants'}>Registrant</Link>
+				</nav>
+				<Switch>
+					<Route path={'/registrants'}>
+						<Registrant />
+					</Route>
+				</Switch>
+			</header>
+		</div>
+	);
 }
 
 export default App;
